c62ec6cf086a2060582f891f2ebd63cd374066df
Marcos Hack <marcoshack@gmail.com>
2012-10-26 14:34:46 -0200

    respond_to {html, json}

--- app/controllers/feiras_controller.rb
+++ app/controllers/feiras_controller.rb
@@ -1,13 +1,28 @@
 class FeirasController < ApplicationController
   def search
     @feiras = Feira.perto_de(params[:lat], params[:lng]).page(params[:p])
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @feiras }
+    end
   end
   
   def show
     @feira = Feira.find_by_id(param[:id])
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @feira }
+    end
   end
   
   def index
     @feiras = Feira.page(params[:p])
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @feiras }
+    end
   end
 end
