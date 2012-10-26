42a2a8eb2302660f3704340059fc8758d92519bf
Luiz Rocha <lsdrocha@gmail.com>
2012-10-26 16:39:26 -0200

    exibir o nome do dia da semana na interface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# encoding: utf-8
 module ApplicationHelper
+
+  def nome_dia_da_semana(feira)
+    {
+      0 => 'domingo',
+      1 => 'segunda',
+      2 => 'terça',
+      3 => 'quarta',
+      4 => 'quinta',
+      5 => 'sexta',
+      6 => 'sábado'
+    }[feira.dia_da_semana]
+  end
+
 end
